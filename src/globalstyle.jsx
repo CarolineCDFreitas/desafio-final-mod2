a2dfc1c9ac2c1c113615fd28cac758cc504e23ac
CarolineCaetano <caroline_bdl@hotmail.com>
2024-11-17 23:21:26 -0300

    add: utilitário e estilo global

---
+++ src/globalstyle.jsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Poppins:wght@400&display=swap');
+
+*{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+
+body{
+font-family: "Poppins", sans-serif;
+font-weight: 400;
+}
+`;
---
+++ src/utils.jsx
@@ -0,0 +1,26 @@
+import { css } from "styled-components";
+
+export const FlexboxCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexboxColumn = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexboxSpaceAround = css`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const FlexboxSpaceEvenly = css`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
