c4ce76b7bd965c81cd4eca6c745a3383294d19e3
CarolineCaetano <caroline_bdl@hotmail.com>
2024-11-24 01:48:09 -0300

    fix style: remoção de border de marcação para layout final e ajuste de margem

--- src/Components/Header/HeaderStyle.jsx
+++ src/Components/Header/HeaderStyle.jsx
@@ -5,9 +5,8 @@ export const HeaderStyled = styled.header`
   ${FlexboxSpaceBetween};
   width: calc(100% - 8rem);
   gap: 2rem;
-  margin: 0.5em 4rem;
   margin: 0 auto;
-  background-color: coral;
+  margin-top: 0.5em;
 
   img {
     width: 7%;
@@ -15,7 +14,7 @@ export const HeaderStyled = styled.header`
   @media (max-width: 480px) {
     width: 100%;
     gap: 1rem;
-    margin: 0;
+    margin: 0.2rem 0 0 0;
 
     img {
       width: 15%;
